b32881b0118ba50038e603c07e90f80e47ad5652
chengyangye <chengyangye@chengyangye-Latitude-E5440.(none)>
2015-05-14 11:22:34 +0800

    del

--- clear_cache.sh
+++ clear_cache.sh
@@ -9,3 +9,7 @@ rm -f ./wx/public_html/admin2/data/runtime/Cache/Home/*.php
 rm -f ./wx/public_html/admin2/data/runtime/Cache/System/*.php
 rm -f ./wx/public_html/admin2/data/runtime/Cache/User/*.php
 rm -f ./wx/public_html/admin2/data/runtime/Cache/Wap/*.php
+
+find -name Thumbs.db |xargs rm -rf
+
+
